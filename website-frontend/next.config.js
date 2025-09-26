8f131c127aeb05735f68e74088863cb35ec545a8
KerryLuo <kerryluo1@gmail.com>
2025-09-25 23:24:33 -0400

    preliminary gallery

---
+++ website-frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'i.imgur.com' },
+      { protocol: 'https', hostname: 'imgur.com' },
+    ],
+  },
+};
+module.exports = nextConfig;
